88e8383778674f1cb482b44d9bc8faacdfca668a
SimpanEU <simon__n@hotmail.com>
2018-04-16 11:31:03 +0200

    Updates

--- src/mimic/StoreTest.java
+++ src/mimic/StoreTest.java
@@ -1,6 +1,6 @@
 package mimic;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
@@ -8,8 +8,21 @@ class StoreTest {
 
 	@Test
 	void test() {
-		fail("Not yet implemented");
+		
+		Store store = new Store();
+		
+		/*
+		store.learnResponse(String request, String response);
+		store.getResponse(String request);
+		store.unlearnAll();
+		*/
+
+		assertEquals(store.learnResponse(x, x), x);
+		assertEquals(store.getResponse(x), x);
+		assertEquals(store.unlearnAll(), x);
+		
+		
+		
 	}
 
 }
-// add something
