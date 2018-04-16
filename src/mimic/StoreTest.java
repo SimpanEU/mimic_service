03626ab16b875eb2276248a3583afbd78b756a86
SimpanEU <simon__n@hotmail.com>
2018-04-16 14:07:08 +0200

    Updates

--- src/mimic/StoreTest.java
+++ src/mimic/StoreTest.java
@@ -6,13 +6,12 @@ import static org.junit.Assert.assertNotEquals;
 import org.junit.jupiter.api.Test;
 
 class StoreTest {
-
-	/*
-	store.learnResponse(String request, String response);
-	store.getResponse(String request);
-	store.unlearnAll();
-	*/
 	
+	/*
+	 * ID 1
+	 * Tests learnResponse function is working by giving "apple" and "fruit" as parameters and calling for getResponse.
+	 * Expected return should be "fruit"
+	 */
 	@Test
 	void test_learnAndGetResponse() {
 		Store store = new Store();
@@ -20,6 +19,12 @@ class StoreTest {
 		assertEquals(store.getResponse("apple"), "fruit");
 	}
 
+	
+	/*
+	 * ID 2
+	 * Tests getResponse function is giving correct return when being asked for an unsaved entry.
+	 * Expected return should be "null"
+	 */
 	@Test
 	void test_getResponseWithoutPrevRecord() {
 		Store store = new Store();
@@ -27,6 +32,12 @@ class StoreTest {
 		assertEquals(store.getResponse(""), null);
 	}
 	
+	
+	/*
+	 * ID 3
+	 * Tests unlearnAll function is working by first learning different responses, ask for learned responses, unlearn the responses and then ask again.
+	 * Expected return should be "null"
+	 */
 	@Test
 	void test_unlearnAll() {
 		Store store = new Store();
@@ -42,11 +53,17 @@ class StoreTest {
 		assertEquals(store.getResponse("pear"), null);
 	}
 	
+	
+	/*
+	 * ID 4
+	 * Tests that learnResponse and getResponse functions is not returning a wrong value.
+	 * Expected return is not null.
+	 */
 	@Test
 	void test_learnAndGetNotMatchingResponse() {
 		Store store = new Store();
 		store.learnResponse("apple", "apple");
-		assertNotEquals(store.getResponse("apple"), "banana");
+		assertNotEquals(store.getResponse("apple"), null);
 	}
 	
 	
