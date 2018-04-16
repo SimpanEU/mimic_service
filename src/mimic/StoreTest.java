3ffbe373aefd3fa787850fc8819ab44ae111a4c6
SimpanEU <simon__n@hotmail.com>
2018-04-16 11:35:55 +0200

    Updates

--- src/mimic/StoreTest.java
+++ src/mimic/StoreTest.java
@@ -6,23 +6,29 @@ import org.junit.jupiter.api.Test;
 
 class StoreTest {
 
+	/*
+	store.learnResponse(String request, String response);
+	store.getResponse(String request);
+	store.unlearnAll();
+	*/
+	
 	@Test
-	void test() {
+	void test_learnResponse() {
 		
 		Store store = new Store();
-		
-		/*
-		store.learnResponse(String request, String response);
-		store.getResponse(String request);
-		store.unlearnAll();
-		*/
-
 		assertEquals(store.learnResponse(x, x), x);
+	}
+		
+	@Test
+	void test_getResponse() {
+		Store store = new Store();
 		assertEquals(store.getResponse(x), x);
+	}
+	
+	@Test
+	void test_unlearnAll() {
+		Store store = new Store();
 		assertEquals(store.unlearnAll(), x);
-		
-		
-		
 	}
 
 }
