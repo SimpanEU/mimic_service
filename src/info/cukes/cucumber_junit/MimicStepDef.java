cc9dd0141a94b2971eb2d8d06e68cb92d723c472
Simp <Simp@Ss.lan>
2018-04-23 01:43:59 +0200

    Updates within MimicStepDef, launches and exits mimic.jar

--- src/info/cukes/cucumber_junit/MimicStepDef.java
+++ src/info/cukes/cucumber_junit/MimicStepDef.java
@@ -1,6 +1,7 @@
 package info.cukes.cucumber_junit;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
@@ -15,44 +16,59 @@ public class MimicStepDef {
 
 	@Given("^mimic\\.jar is running$")
 	public void mimic_jar_is_running() throws Throwable {
-		String request=host+"unlearn";
+		
+		// Launches mimic.jar from your C:/Users/<username> folder.
+		String user = System.getProperty("user.name");
+		String cmd = "java -jar C://Users//" + user + "//mimic.jar";
+		Runtime.getRuntime().exec(cmd);
+		
+		// Tests if LearnNextResponse method returns "OK".
+		String request=host+"LearnNextResponse?text=TestIfRunning";
 		String response=service.executeGetRequest(request);
 		assertEquals(response, "OK");
 	}
 
+	
 	@When("^I learn a reponse$")
 	public void i_learn_a_reponse() throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
 	}
-
 	@When("^I request a reponse$")
 	public void i_request_a_reponse() throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
 	}
-
 	@Then("^mimic\\.jar is responding with correct response$")
 	public void mimic_jar_is_responding_with_correct_response() throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
 	}
-
 	@When("^I unlearn the previous request/response using the \"([^\"]*)\" command$")
 	public void i_unlearn_the_previous_request_response_using_the_command(String arg1) throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
 	}
-
 	@Then("^Previous request/response is removed$")
 	public void previous_request_response_is_removed() throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
 	}
 
+	
+	
 	@When("^I use the mimic\\.jar shutdown function$")
 	public void i_use_the_mimic_jar_shutdown_function() throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
+		
+		// Triggers KillMimic method and shuts down the service. Expected return is "OK".
+		String request=host+"KillMimic";
+		String response=service.executeGetRequest(request);
+		assertEquals(response, "OK");
 	}
 
 	@Then("^mimic\\.jar is not running$")
 	public void mimic_jar_is_not_running() throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
+		
+		// Calls for KillMimic method once again, this time we do not expect any "OK" as the service should already been shutdown.
+		String request=host+"KillMimic";
+		String response=service.executeGetRequest(request);
+		assertNotEquals(service.executeGetRequest(request), "OK");
+		
+		/*
+		// Re-launches the mimic.jar after its being shutdown, requires you to have the mimic.jar in your C:/Users/<username> folder.
+		String user = System.getProperty("user.name");
+		String cmd = "java -jar C://Users//" + user + "//mimic.jar";
+		Runtime.getRuntime().exec(cmd); */
 	}
 
 }
