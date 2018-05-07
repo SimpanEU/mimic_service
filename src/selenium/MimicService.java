77a5e117ecf121745bd66c4702e4834456dcdc4a
saadypk <saadypk@saadypk-ThinkPad-T430>
2018-05-07 13:52:17 +0200

    Updating selenium

--- src/selenium/MimicService.java
+++ src/selenium/MimicService.java
@@ -1,9 +1,60 @@
 package selenium;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class MimicService {
-
-	public MimicService() {
-		// TODO Auto-generated constructor stub
+	
+	private WebDriver webDriver;
+	
+	public void mimicservice() {
+		System.setProperty("webdriver.chrome.driver", "bin/chromedriver");		
+		webDriver = new ChromeDriver();
+		webDriver.get("localhost:8080/request");
+		WebElement element = webDriver.findElement(By.xpath("//textarea[@name='text']"));
+		element.sendKeys("res1");
+		WebElement element2 = webDriver.findElement(By.id("learn"));
+		element2.click();
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		webDriver.get("localhost:8080/LearnNextResponse?text=res2");
+		webDriver.get("localhost:8080/request");
+		webDriver.get("localhost:8080/LearnNextResponse?text=res3");
+		webDriver.get("localhost:8080/resquest");
+		webDriver.get("localhost:8080/LearnNextResponse?text=test4");
+		webDriver.get("localhost:8080/request");
+		webDriver.get("localhost:8080/LearnNextResponse?text=test5");
+		webDriver.get("localhost:8080/request");
+		
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		webDriver.get("localhost:8080/request");
+		webDriver.get("localhost:8080/request");
+		webDriver.get("localhost:8080/request");
+		webDriver.get("localhost:8080/unlearn");
+		webDriver.get("localhost:8080/resetState");
+		webDriver.get("localhost:8080/request");
+		webDriver.get("localhost:8080/request");
+		webDriver.get("localhost:8080/request");
+		
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
 	}
-
+	
 }
--- src/selenium/MimicServiceTest.java
+++ src/selenium/MimicServiceTest.java
@@ -1,9 +1,13 @@
 package selenium;
 
+import org.junit.jupiter.api.Test;
+
 public class MimicServiceTest {
 
-	public MimicServiceTest() {
-		// TODO Auto-generated constructor stub
+	@Test
+	public void test() {
+		MimicService mimictest = new MimicService();
+		mimictest.mimicservice();
 	}
-
+	
 }
